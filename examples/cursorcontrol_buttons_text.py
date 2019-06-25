979199710e7efd147b33e790fb51c1630fb97ea3
brentru <robots199@me.com>
2019-06-25 13:47:40 -0400

    update cursorcontrol_btns_txt to dynamically resize btns/ui elements for pygamer's smaller display

--- examples/cursorcontrol_buttons_text.py
+++ examples/cursorcontrol_buttons_text.py
@@ -1,15 +1,11 @@
 import time
 import board
 import displayio
-from adafruit_bitmap_font import bitmap_font
 from adafruit_button import Button
 from adafruit_cursorcontrol.cursorcontrol import Cursor
 from adafruit_cursorcontrol.cursorcontrol_cursormanager import CursorManager
 from adafruit_display_text import label
-
-# Load the font
-THE_FONT = "/fonts/Arial-12.bdf"
-font = bitmap_font.load_font(THE_FONT)
+import terminalio
 
 # Create the display
 display = board.DISPLAY
@@ -17,10 +13,13 @@ display = board.DISPLAY
 # Create the display context
 splash = displayio.Group(max_size=22)
 
+# Use the built-in system font
+font = terminalio.FONT
+
 ##########################################################################
 # Make a background color fill
 
-color_bitmap = displayio.Bitmap(320, 240, 1)
+color_bitmap = displayio.Bitmap(display.width, display.height, 1)
 color_palette = displayio.Palette(1)
 color_palette[0] = 0x404040
 bg_sprite = displayio.TileGrid(color_bitmap,
@@ -30,32 +29,44 @@ splash.append(bg_sprite)
 
 ##########################################################################
 
-# Set up button size info
+# Set up button/label properties
 BUTTON_WIDTH = 80
 BUTTON_HEIGHT = 40
 BUTTON_MARGIN = 20
+LBL_HEADER = [100, 20]
+LBL_TEXT = [120, 40]
+
+# Resize buttons for small display (PyGamer)
+if display.width < 240:
+    BUTTON_WIDTH = int(BUTTON_WIDTH / 2)
+    BUTTON_HEIGHT = int(BUTTON_HEIGHT / 2)
+    BUTTON_MARGIN = int(BUTTON_MARGIN / 2)
+    LBL_HEADER[0] -= 75
+    LBL_HEADER[1] -= 10
+    LBL_TEXT[0] -= 70
+    LBL_TEXT[1] += 55
 
 # Create the buttons
 buttons = []
 
 button_speed_inc = Button(x=BUTTON_MARGIN, y=BUTTON_MARGIN+BUTTON_HEIGHT,
                           width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
-                          label="+ Speed", label_font=font)
+                          label="Speed+", label_font=font)
 buttons.append(button_speed_inc)
 
 button_speed_dec = Button(x=BUTTON_MARGIN, y=BUTTON_MARGIN*4+BUTTON_HEIGHT,
                           width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
-                          label="- Speed", label_font=font)
+                          label="Speed-", label_font=font)
 buttons.append(button_speed_dec)
 
 button_scale_pos = Button(x=BUTTON_MARGIN*3+2*BUTTON_WIDTH, y=BUTTON_MARGIN+BUTTON_HEIGHT,
                           width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
-                          label="+ Scale", label_font=font, style=Button.SHADOWRECT)
+                          label="Scale+", label_font=font, style=Button.SHADOWRECT)
 buttons.append(button_scale_pos)
 
 button_scale_neg = Button(x=BUTTON_MARGIN*3+2*BUTTON_WIDTH, y=BUTTON_MARGIN*4+BUTTON_HEIGHT,
                           width=BUTTON_WIDTH, height=BUTTON_HEIGHT,
-                          label="- Scale", label_font=font, style=Button.SHADOWRECT)
+                          label="Scale-", label_font=font, style=Button.SHADOWRECT)
 buttons.append(button_scale_neg)
 
 # Show the button
@@ -64,15 +75,15 @@ for b in buttons:
 
 # Create a text label
 text_label = label.Label(font, text="CircuitPython Cursor!", color=0x00FF00,
-                         x = 100, y = 20)
+                         x = LBL_HEADER[0], y = LBL_HEADER[1])
 splash.append(text_label)
 
 text_speed = label.Label(font, max_glyphs = 15, color=0x00FF00,
-                         x = 120, y = 40)
+                         x = LBL_TEXT[0], y = LBL_TEXT[1])
 splash.append(text_speed)
 
 text_scale = label.Label(font, max_glyphs = 15, color=0x00FF00,
-                         x = 120, y = 60)
+                         x = LBL_TEXT[0], y = LBL_TEXT[1]+20)
 splash.append(text_scale)
 
 # initialize the mouse cursor object
@@ -105,4 +116,4 @@ while True:
         prev_btn.selected = False
     text_speed.text = 'Speed: {0}px'.format(mouse_cursor.speed)
     text_scale.text = 'Scale: {0}px'.format(mouse_cursor.scale)
-    time.sleep(0.01)
+    time.sleep(0.1)
